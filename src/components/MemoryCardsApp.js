b9c7cfbbfb648cd7be03cab1272ddb62d44aa636
jacobwmorris <jacobwmorris0@gmail.com>
2023-05-02 16:40:59 -0400

    Implement Scoreboard

--- src/components/MemoryCardsApp.js
+++ src/components/MemoryCardsApp.js
@@ -1,7 +1,25 @@
+import {useState} from "react"
 import Scoreboard from "./Scoreboard"
 import CardRandomizer from "./CardRandomizer"
 
 function MemoryCardsApp() {
+  const [score, setScore] = useState(0)
+  const [record, setRecord] = useState(0)
+  const [usedCards, setUsedCards] = useState([])
+  const [cards, setCards] = useState(pastaCardInfo.slice())
+  const [isGameFinished, setIsGameFinished] = useState(true)
+
+  //Callbacks
+  function handleReset(event) {
+    setScore(0)
+    setUsedCards([])
+    setIsGameFinished(false)
+    //shuffle cards
+  }
+  
+  //Rendering
+  const isPerfectScore = (score === cards.length)
+
   return (
     <div>
       <div>
@@ -13,11 +31,26 @@ function MemoryCardsApp() {
             <li>Remember each card you click, and don't click the same one twice!</li>
           </ul>
         </div>
-        <Scoreboard/>
+        <Scoreboard
+          score={score}
+          record={record}
+          isGameFinished={isGameFinished}
+          isPerfectScore={isPerfectScore}
+          handleReset={handleReset}
+        />
       </div>
       <CardRandomizer/>
     </div>
   )
 }
+
+const pastaCardInfo = [
+  {name: "spaghetti", id: 0},  {name: "fettuccine", id: 1},
+  {name: "lasagna", id: 2},    {name: "ziti", id: 3},
+  {name: "macaroni", id: 4},   {name: "ravioli", id: 5},
+  {name: "penne", id: 6},      {name: "rigatoni", id: 7},
+  {name: "tortellini", id: 8}, {name: "cavatappi", id: 9},
+  {name: "farfalle", id: 10},  {name: "gnocci", id: 11}
+]
 
 export default MemoryCardsApp
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,8 +1,25 @@
 
-function Scoreboard({score, record, isGameFinished, isPerfectScore}) {
+function Scoreboard({score, record, isGameFinished, isPerfectScore, handleReset}) {
   return (
-    <div>(Scoreboard)</div>
+    <div>
+      <p>Score: {score}</p>
+      <p>Record: {record}</p>
+      {
+        isGameFinished ? (
+        <>
+          <p>{endMessage(isPerfectScore)}</p>
+          <button type="button" onClick={handleReset}>Restart</button>
+        </>) : null
+      }
+    </div>
   )
+}
+
+function endMessage(isPerfectScore) {
+  if (isPerfectScore) {
+    return <p>Congratulations, you got a perfect score!</p>
+  }
+  return <p>Game over!</p>
 }
 
 export default Scoreboard
